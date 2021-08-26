642731ee978ca5cd04113e6f3dc3b88b26c5f951
Ivan Terehin <ITerex91@yandex.ru>
2021-08-26 10:59:57 +0300

    update setup.cfg per-file-ignores

--- billing_api/main.py
+++ billing_api/main.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8008,
+        port=8008,  # type: ignore
         log_config=LOGGING,
         log_level=logging.DEBUG,
         reload=True,
